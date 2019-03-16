e13715c02894aa40ef01fd9661e5427bf594fafb
Maggie <maggie.fm.appleton@gmail.com>
2019-03-16 18:09:47 +0000

    things

--- src/components/subbutton.js
+++ src/components/subbutton.js
@@ -5,21 +5,21 @@ import { css } from "@emotion/core";
 class Subbutton extends React.Component {
     render() {
         return (
-            <Link
-                css={css({
-                    boxSizing: "border-box",
-                    borderRadius: "4px",
-                    backgroundColor: "#3FCCDC",
-                    color: "#fff",
-                    padding: "15px 20px",
-                    fontWeight: "700",
-                    float: "right"
-                })}
-                to="/"
-            >
-                Subscribe
-            </Link>
-        )}
+          <Link
+            css={css({
+              boxSizing: "border-box",
+              borderRadius: "4px",
+              backgroundColor: "#3FCCDC",
+              color: "#fff",
+              padding: "15px 20px",
+              fontWeight: "700",
+              float: "right"
+            })}
+            to={`/`}
+          >
+            Subscribe
+          </Link>
+        );}
      }
 
 export default Subbutton
