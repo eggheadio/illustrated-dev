e3146fa94445dcac8073c367a19abb7686ef323e
Appleton <maggie.fm.appleton@gmail.com>
2019-04-10 23:19:12 +0100

    Updated API article to be interactive (#21)
    * broken things
    
    * viola
    
    * updated api article to be interactive

--- src/pages/about_seperate.js
+++ src/pages/about_seperate.js
@@ -1,17 +1,15 @@
-import React from "react";
-import SEO from "../components/seo";
+import React from 'react'
+import SEO from '../components/seo'
 
 const AboutPage = () => (
   <main
     css={{
-      margin: "100px",
-      fontFamily: "ff-tisa-web-pro"
-    }}
-  >
-    <SEO title="404: Not found" />
+      margin: '100px',
+      fontFamily: 'ff-tisa-web-pro',
+    }}>
+    <SEO title='The About Page' />
     <h1>About</h1>
-    <p>Things</p>
   </main>
-);
+)
 
-export default AboutPage;
+export default AboutPage
