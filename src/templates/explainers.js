2d4cb0a11bd65c2b7cb9d8b12a42e6eadfc723a9
Maggie <maggie@egghead.io>
2020-01-02 16:06:13 +0000

    tweak

--- src/templates/explainers.js
+++ src/templates/explainers.js
@@ -6,7 +6,7 @@ import { MDXRenderer } from 'gatsby-plugin-mdx'
 import mdxComponents from '../components/mdx'
 import Link from '../components/link'
 import Container from '../components/container'
-import { bpMinMD, bpMinSM } from '../utils/breakpoints'
+import { bpMinSM } from '../utils/breakpoints'
 import SEO from '../components/seo'
 import get from 'lodash/get'
 import Share from '../components/share'
@@ -28,158 +28,157 @@ class ExplainerTemplate extends React.Component {
           image={thumbnail && `https://illustrated.dev${thumbnail.src}`}
           title={explainer.frontmatter.title}
         />
-          <Container
-            noVerticalPadding
+        <Container
+          noVerticalPadding
+          css={css({
+            [bpMinSM]: {
+              marginTop: '50px',
+            },
+            marginTop: 0,
+          })}>
+          <div
             css={css({
-              [bpMinSM]: {
-                marginTop: '50px',
-              },
-              marginTop: 0,
+              background: 'white',
+              [bpMinSM]: { padding: '30px 20px', margin: '0' },
+              padding: '10px 20px',
+              marginTop: '40px',
+              borderRadius: '2px',
+              boxShadow: '0px 1px 2px rgba(52, 61, 68, 0.1)',
+              clear: 'both',
             })}>
             <div
               css={css({
-                background: 'white',
-                [bpMinSM]: { padding: '30px 20px', margin: '0' },
-                padding: '10px 20px',
-                marginTop: '40px',
-                borderRadius: '2px',
-                boxShadow: '0px 1px 2px rgba(52, 61, 68, 0.1)',
-                clear: 'both',
+                maxWidth: '780px',
+                margin: '0 auto',
               })}>
+              <p
+                css={css({
+                  color: '#7D8EB0',
+                  textAlign: 'center',
+                  fontSize: '1em',
+                })}>
+                {explainer.frontmatter.date}
+              </p>
+              <h1
+                css={css({
+                  [bpMinSM]: {
+                    fontSize: '2.8rem',
+                  },
+                  fontSize: '2.1rem',
+                  textAlign: 'center',
+                  fontWeight: '100',
+                  marginBottom: '40px',
+                  marginTop: '20px',
+                  fontFamily: 'ff-tisa-web-pro, serif',
+                })}>
+                {explainer.frontmatter.title}
+              </h1>
+
+              <MDXProvider components={mdxComponents}>
+                <MDXRenderer>{explainer.body}</MDXRenderer>
+              </MDXProvider>
+              <Share
+                socialConfig={{
+                  twitterHandle: site.siteMetadata.author,
+                  config: {
+                    url: `https://illustrated.dev/${explainer.frontmatter.slug}`,
+                    title: explainer.frontmatter.title,
+                    media: `${image && `https://illustrated.dev${image.src}`}`,
+                  },
+                }}
+                tags={explainer.frontmatter.tags}
+              />
+            </div>
+          </div>
+          <div
+            css={css({
+              [bpMinSM]: {
+                display: 'grid',
+                gridTemplateColumns: previous && '1fr 2fr',
+              },
+              display: 'flex',
+              flexDirection: 'column-reverse',
+            })}>
+            {previous && (
               <div
                 css={css({
-                  maxWidth: '780px',
-                  margin: '0 auto',
+                  gridColumnStart: '1',
+                  margin: '80px 40px 0 0',
+                  paddingRight: '40px',
+                  borderRight: '1px solid hsla(200, 10%, 80%, 0.7)',
+                  textAlign: 'right',
+                  h4: {
+                    fontFamily: 'freight-sans-pro, sans-serif',
+                    textTransform: 'uppercase',
+                    fontSize: '16px',
+                    color: 'hsla(200, 10%, 30%, 0.7)',
+                  },
                 })}>
-                <p
-                  css={css({
-                    color: '#7D8EB0',
-                    textAlign: 'center',
-                    fontSize: '1em',
-                  })}>
-                  {explainer.frontmatter.date}
-                </p>
-                <h1
-                  css={css({
-                    [bpMinSM]: {
-                      fontSize: '2.8rem',
-                    },
-                    fontSize: '2.1rem',
-                    textAlign: 'center',
-                    fontWeight: '100',
-                    marginBottom: '40px',
-                    marginTop: '20px',
-                    fontFamily: 'ff-tisa-web-pro, serif',
-                  })}>
-                  {explainer.frontmatter.title}
-                </h1>
-
-                <MDXProvider components={mdxComponents}>
-                  <MDXRenderer>{explainer.body}</MDXRenderer>
-                </MDXProvider>
-                <Share
-                  socialConfig={{
-                    twitterHandle: site.siteMetadata.author,
-                    config: {
-                      url: `https://illustrated.dev/${explainer.frontmatter.slug}`,
-                      title: explainer.frontmatter.title,
-                      media: `${image &&
-                        `https://illustrated.dev${image.src}`}`,
-                    },
-                  }}
-                  tags={explainer.frontmatter.tags}
-                />
+                <Link to={`/${previous.frontmatter.slug}`} rel='previous'>
+                  <h4
+                    css={css({
+                      opacity: '0.6',
+                    })}>
+                    previous
+                  </h4>
+                  <h2
+                    css={css({
+                      opacity: '0.8',
+                      fontWeight: '100',
+                      fontFamily: 'ff-tisa-web-pro, serif',
+                      fontSize: '1.5em',
+                      fontStyle: 'italic',
+                      marginBottom: '40px',
+                    })}>
+                    <svg
+                      width='20px'
+                      height='20.9px'
+                      viewBox='0 0 12.3 20.9'
+                      style={{ fill: '#40C4D4', paddingTop: '3px' }}>
+                      <polygon points='9.3,15.1 5,10.5 9.3,5.8 12.3,2.7 9.8,0 4.3,5.8 0,10.5 4.3,15.1 9.8,20.9 12.3,18.3 ' />
+                    </svg>{' '}
+                    {previous.frontmatter.title}
+                  </h2>
+                </Link>
               </div>
-            </div>
-            <div
-              css={css({
-                [bpMinSM]: {
-                  display: 'grid',
-                  gridTemplateColumns: previous && '1fr 2fr',
-                },
-                display: 'flex',
-                flexDirection: 'column-reverse',
-              })}>
-              {previous && (
-                <div
-                  css={css({
-                    gridColumnStart: '1',
-                    margin: '80px 40px 0 0',
-                    paddingRight: '40px',
-                    borderRight: '1px solid hsla(200, 10%, 80%, 0.7)',
-                    textAlign: 'right',
-                    h4: {
-                      fontFamily: 'freight-sans-pro, sans-serif',
-                      textTransform: 'uppercase',
-                      fontSize: '16px',
-                      color: 'hsla(200, 10%, 30%, 0.7)',
-                    },
-                  })}>
-                  <Link to={`/${previous.frontmatter.slug}`} rel='previous'>
-                    <h4
-                      css={css({
-                        opacity: '0.6',
-                      })}>
-                      previous
-                    </h4>
-                    <h2
-                      css={css({
-                        opacity: '0.8',
-                        fontWeight: '100',
-                        fontFamily: 'ff-tisa-web-pro, serif',
-                        fontSize: '1.5em',
-                        fontStyle: 'italic',
-                        marginBottom: '40px',
-                      })}>
-                      <svg
-                        width='20px'
-                        height='20.9px'
-                        viewBox='0 0 12.3 20.9'
-                        style={{ fill: '#40C4D4', paddingTop: '3px' }}>
-                        <polygon points='9.3,15.1 5,10.5 9.3,5.8 12.3,2.7 9.8,0 4.3,5.8 0,10.5 4.3,15.1 9.8,20.9 12.3,18.3 ' />
-                      </svg>{' '}
-                      {previous.frontmatter.title}
-                    </h2>
-                  </Link>
-                </div>
-              )}
-              {next && (
-                <div
-                  css={css({
-                    gridColumnStart: '2',
-                    marginTop: '80px',
+            )}
+            {next && (
+              <div
+                css={css({
+                  gridColumnStart: '2',
+                  marginTop: '80px',
 
-                    h4: {
-                      fontFamily: 'freight-sans-pro, sans-serif',
-                      textTransform: 'uppercase',
-                      fontSize: '16px',
-                      color: 'hsla(0, 0%, 0%, 0.5)',
-                    },
-                  })}>
-                  <Link to={`/${next.frontmatter.slug}`} rel='next'>
-                    <h4>next</h4>
-                    <h2
-                      css={css({
-                        fontWeight: '100',
-                        fontFamily: 'ff-tisa-web-pro, serif',
-                        fontSize: '2.2em',
-                        fontStyle: 'italic',
-                        marginBottom: '80px',
-                      })}>
-                      {next.frontmatter.title}{' '}
-                      <svg
-                        width='20.1px'
-                        height='40px'
-                        viewBox='0 0 20.1 40'
-                        style={{ fill: '#40C4D4', paddingTop: '18px' }}>
-                        <polygon points='12.7,9.9 3.4,0 0,3.6 5.9,9.9 13.3,17.7 5.9,25.6 0,31.9 3.4,35.5 12.7,25.6 20.1,17.7 ' />
-                      </svg>
-                    </h2>
-                  </Link>
-                </div>
-              )}
-            </div>
-          </Container>
+                  h4: {
+                    fontFamily: 'freight-sans-pro, sans-serif',
+                    textTransform: 'uppercase',
+                    fontSize: '16px',
+                    color: 'hsla(0, 0%, 0%, 0.5)',
+                  },
+                })}>
+                <Link to={`/${next.frontmatter.slug}`} rel='next'>
+                  <h4>next</h4>
+                  <h2
+                    css={css({
+                      fontWeight: '100',
+                      fontFamily: 'ff-tisa-web-pro, serif',
+                      fontSize: '2.2em',
+                      fontStyle: 'italic',
+                      marginBottom: '80px',
+                    })}>
+                    {next.frontmatter.title}{' '}
+                    <svg
+                      width='20.1px'
+                      height='40px'
+                      viewBox='0 0 20.1 40'
+                      style={{ fill: '#40C4D4', paddingTop: '18px' }}>
+                      <polygon points='12.7,9.9 3.4,0 0,3.6 5.9,9.9 13.3,17.7 5.9,25.6 0,31.9 3.4,35.5 12.7,25.6 20.1,17.7 ' />
+                    </svg>
+                  </h2>
+                </Link>
+              </div>
+            )}
+          </div>
+        </Container>
       </>
     )
   }
