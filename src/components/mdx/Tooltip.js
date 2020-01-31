f808729c803f25911959583a5453e74a90f0c3f7
Maggie <maggie@egghead.io>
2020-01-31 23:25:40 +0000

    added tippy.js

---
+++ src/components/mdx/Tooltip.js
@@ -0,0 +1,22 @@
+import React, { forwardRef } from 'react'
+import Tippy from '@tippy.js/react'
+import 'tippy.js/dist/tippy.css'
+import 'tippy.js/animations/shift-away.css'
+
+const Tooltip = forwardRef((props, ref) => {
+  return (
+    <Tippy
+      duration='500'
+      interactive='true'
+      animation='shift-away'
+      content={props.tiptext}>
+      <button ref={ref}>{props.children}</button>
+    </Tippy>
+  )
+})
+
+export default Tooltip
+
+// To use in any MDX file, add 'import Tooltip from '../../../src/components/mdx/Tooltip.js' to the top.
+
+// Then add <Tooltip tiptext="I'm working right?">Hello</Tooltip> anywhere in the body
