941319773a6b039ad0087f88d38f397fe34a7559
Maggie <maggie@egghead.io>
2020-02-10 08:48:11 +0000

    fixed FAQ page layout

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -27,23 +27,14 @@ export default function FAQPage() {
           css={css({
             background: 'white',
             padding: '30px 60px',
-            borderRadius: '2px',
-            boxShadow: '0px 1px 2px rgba(52, 61, 68, 0.1)',
             clear: 'both',
             margin: '0 auto',
             h3: { fontWeight: '500', marginTop: '2em' },
           })}>
           <div
             css={css({
-              [bpMinSM]: {
-                padding: '15px',
-              },
-              background: 'white',
-              padding: '30px 60px',
-              borderRadius: '2px',
-              boxShadow: '0px 1px 2px rgba(52, 61, 68, 0.1)',
-              clear: 'both',
               margin: '0 auto',
+              maxWidth: '680px',
             })}>
             <FAQ />
           </div>
