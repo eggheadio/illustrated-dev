7fa2b695549df0960f07b7a1f935894f7213de38
Maggie <maggie@egghead.io>
2020-02-22 20:11:03 +0000

    removed extra imports from mdx posts

--- src/components/mdx/index.js
+++ src/components/mdx/index.js
@@ -14,6 +14,7 @@ import TwoCol from './TwoCol'
 import Link from '../Link'
 import NotesContainer from './NotesContainer'
 import FullNote from './FullNote'
+import SmallCenter from './SmallCenter'
 
 export default {
   h1: props => <Title {...props} />,
@@ -31,6 +32,7 @@ export default {
   Tooltip: props => <Tooltip {...props} />,
   Link: props => <Link {...props} />,
   TwoCol: props => <TwoCol {...props} />,
+  SmallCenter: props => <SmallCenter {...props} />,
   NotesContainer: props => <NotesContainer {...props} />,
   FullNote: props => <FullNote {...props} />,
   pre: preProps => {
