0e0a7e3f14e7c297d5619f867e89cff934a4e761
Maggie <maggie@egghead.io>
2020-02-22 09:08:17 +0000

    Fixed MDX importing

---
+++ src/components/mdx/Subheaders.js
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled'
+
+const H2 = styled.h2`
+  text-align: left;
+  margin-top: 60px;
+  margin-bottom: 20px;
+  font-size: 2em;
+  font-weight: 400;
+  line-height: 1.1;
+  font-family: 'freight-sans-pro', -apple-system, sans-serif;
+`
+
+const H3 = styled.h3``
+
+const H4 = styled.h4``
+
+const H5 = styled.h5`
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+`
+const H6 = styled.h6``
+
+export { H2, H3, H4, H5, H6 }
